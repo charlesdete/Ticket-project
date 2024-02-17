3902486754c9ea802ef205161549ab1ff515e661
charles dete <charlesdete52@gmail.com>
2024-02-17 11:56:40 +0300

    fix: rename conflicting file-name

--- api/dashboard.php
+++ api/dashboard.php
@@ -11,7 +11,7 @@
 <head>
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
 <title>Dashboard</title>
-<link rel="stylesheet" href="dashboard.css">
+<link rel="stylesheet" href="dashboard-style.css">
 <!--lato font -->
 <link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap" rel="stylesheet">
 <!--matrial icons-->
--- api/footer.php
+++ api/footer.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="footer.css">
+<link rel="stylesheet" href="footer-style.css">
 <footer>
          
             <div class="footer_socials">
